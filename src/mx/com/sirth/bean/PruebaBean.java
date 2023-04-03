e1c9b239c96adff402702883aa05ca743948ae47
Armando <Armando@Arman-PC>
2023-04-03 14:32:21 -0500

    commit

--- src/mx/com/sirth/bean/PruebaBean.java
+++ src/mx/com/sirth/bean/PruebaBean.java
@@ -1,23 +1,7 @@
 package mx.com.sirth.bean;
 
-
-
 public class PruebaBean {
-<<<<<<< HEAD
 	
 
 
-=======
-	private String ar = null;
-
-	public String getAr() {
-		return ar;
-	}
- 
-	public void setAr(String ar) {
-		this.ar = ar;
-	}
-	
-	
->>>>>>> branch 'master' of https://github.com/ArmanGaRi/sirthWeb.git
 }
