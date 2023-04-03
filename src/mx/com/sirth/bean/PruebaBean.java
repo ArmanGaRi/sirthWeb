bda961a43e4e2add94d7d4a8b8c686e5f69c4d60
Rulobike <rulo_r.gr@hotmail.com>
2023-04-03 14:29:04 -0500

    commit

--- src/mx/com/sirth/bean/PruebaBean.java
+++ src/mx/com/sirth/bean/PruebaBean.java
@@ -5,17 +5,10 @@ package mx.com.sirth.bean;
 public class PruebaBean {
 	private String ar = null;
 
-	
-	
-	
-	
-	
-	
-	
 	public String getAr() {
 		return ar;
 	}
-
+ 
 	public void setAr(String ar) {
 		this.ar = ar;
 	}
